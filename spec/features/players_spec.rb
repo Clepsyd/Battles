be79fa62c56b210037986d6d6fa01216d995116f
Guillaume <liard.guillaume@gmail.com>
2019-08-08 16:09:03 +0100

    Test for HP

--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -1,9 +1,21 @@
-feature 'Players name' do
-  scenario 'Can expect players to fill in their names' do
+feature 'Players' do
+  before do
     visit('/')
     fill_in('Player_1_name', with: 'Mario')
     fill_in('Player_2_name', with: 'Guillaume')
     click_on("Submit")
-    expect(page).to have_content "Mario vs. Guillaume"
+  end
+
+  feature 'names' do
+    scenario 'Can expect players to fill in their names' do
+      expect(page).to have_content "Mario vs. Guillaume"
+    end
+  end
+
+  feature 'hit points' do
+    scenario 'Can expect players to see hit points' do
+      expect(page).to have_content "HP: 50", "HP: 50"
+    end
   end
 end
+
