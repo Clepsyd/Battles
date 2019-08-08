75250a96a23759b73cae12f3f1f34d3cfbaaf37e
Mario Garcia Pinan <jaitoone@gmail.com>
2019-08-08 16:00:37 +0100

    Implemented redirection

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-
+  enable :sessions
   get "/" do
     erb :index_get
   end
 
-  post "/" do
-    @p1name = params[:Player_1_name]
-    @p2name = params[:Player_2_name]
-    erb :index_post
+  post "/names" do
+    session[:P1name] = params[:Player_1_name]
+    session[:P2name] = params[:Player_2_name]
+    redirect '/play'
   end
+
+  get "/play" do
+    @p1name = session[:P1name]
+    @p2name = session[:P2name]
+    erb :play
+  end
+
 
   run! if app_file == $0
 
